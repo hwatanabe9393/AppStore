55f03cf9116c4ba6bb819d5ef1a5e76f0c2e4ea1
Hikaru Watanabe <hikaruwatanabe@hikarus-mbp.attlocal.net>
2019-03-20 20:30:53 -0500

    Initial Commit

---
+++ AppStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by Hikaru Watanabe on 3/20/19.
+//  Copyright © 2019 Hikaru Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
